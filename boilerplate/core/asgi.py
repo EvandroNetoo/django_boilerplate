016c64f91e7563103a29e825c844cd1d4055a46d
Evandro Neto <evandro.rsneto@gmail.com>
2024-11-28 11:15:40 -0300

    Adicionando uvicorn e blacknoise

--- boilerplate/core/asgi.py
+++ boilerplate/core/asgi.py
@@ -1,16 +1,13 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
+from pathlib import Path
 
+from blacknoise import BlackNoise
+from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+BASE_DIR = Path(__file__).parent
+
+application = BlackNoise(get_asgi_application())
+application.add(settings.STATIC_ROOT, settings.STATIC_URL)
