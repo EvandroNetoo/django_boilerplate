4fbd65c613bb6c80f82262c453cded2bbd410ab9
Evandro Neto <evandro.rsneto@gmail.com>
2024-12-05 12:34:42 -0300

    removendo favicon

--- boilerplate/core/urls.py
+++ boilerplate/core/urls.py
@@ -1,16 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
-from django.templatetags.static import static
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path(
-        'favicon.ico',
-        RedirectView.as_view(
-            url=static('general/img/favicon.ico'), permanent=True
-        ),
-    ),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('payments/', include('payments.urls')),
